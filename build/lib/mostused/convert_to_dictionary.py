fff522a7bddc4f48b6afa935c21a52524056f4e3
Mackai Curtis <mac.cheese324@gmail.com>
2025-11-24 12:37:55 -0500

    updated setup file

--- build/lib/mostused/convert_to_dictionary.py
+++ build/lib/mostused/convert_to_dictionary.py
@@ -15,6 +15,15 @@ def correct_type(val: str):
         case "None":
             return None
     
+    if val.count(",") > 0:
+        lst = []
+        values = val.split(",")
+        if values[-1] == '':
+            values.pop()
+        for v in values:
+            lst.append(correct_type(v))
+        return lst
+    
     if val.isdigit():
         return int(val)
     try:
@@ -40,8 +49,20 @@ def get_data(file_name: str, key_val_sep: str, sep: str) -> list[dict[str, any]]
 def make_line(d: dict[str, any], key_val_sep: str, sep: str) -> str:
     res = ""
     for key in d:
-        res += key + key_val_sep + str(d[key]) + sep
+        if type(d[key]) != list:
+            res += key + key_val_sep + str(d[key]) + sep
+        else:
+            res += key + key_val_sep + convert_list_to_string(d[key]) + sep
     return res[:-1]
+
+def convert_list_to_string(lst: list) -> str:
+    if len(lst) == 1:
+        return str(lst[0]) + ","
+    else:
+        string = ""
+        for val in lst:
+            string += str(val) + ","
+        return string[:-1]
 
 def write_data(file_name: str, data: list[dict[str, any]], key_val_sep: str, sep: str) -> None:
     w = ""
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mostused",
-    version="1.11",
+    version="1.12",
     description="to reduce recreating code and to make my life easier",
     author="mackai curtis",
     author_email="mac.cheese324@gmail.com",
