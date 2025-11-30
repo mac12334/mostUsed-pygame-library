82cdb5cc022c0f517ab6802fbd2c5100abdb93a2
Mackai Curtis <mac.cheese324@gmail.com>
2025-11-30 13:05:29 -0500

    setup file fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mostused",
-    version="1.12",
+    version="1.12.1",
     description="to reduce recreating code and to make my life easier",
     author="mackai curtis",
     author_email="mac.cheese324@gmail.com",
